2c7f05b14f7fbfca163763f19cd3579f2d12a7de
francois <francoisrvespa@gmail.com>
2014-07-23 19:51:39 +0200

    cleaning code

--- test/assert.js
+++ test/assert.js
@@ -1,14 +1,26 @@
+/**
+
+  Assert with a message
+  #####################
+
+  # Abstract
+
+  Assert @assertion. Display @message in red if assert failed, in green otherwise
+
+
+**/
+
 var assert = require('assert');
 var colors = require('colors');
 
-module.exports = function (name, assertion) {
+module.exports = function (message, assertion) {
   try {
     assert(assertion);
 
-    console.log(' ✓ %s'.green, name);
+    console.log(' ✓ %s'.green, message);
   }
   catch ( error ) {
-    console.log(' × %s'.red, name, error);
+    console.log(' × %s'.red, message, error);
 
     throw error;
   }
