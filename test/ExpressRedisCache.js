fe6f1cad753ea7464a08a7e7d7a4abaf129e225e
francois <francoisrvespa@gmail.com>
2015-01-20 23:22:44 +0100

    test

--- test/ExpressRedisCache.js
+++ test/ExpressRedisCache.js
@@ -14,12 +14,18 @@
   var host      =   'localhost';
   var port      =   6379;
 
+  /** Unit test index.js */
+
   describe ( 'Module', function () {
+
+    /** index.js should expose a function */
 
     it ( 'should be a function', function () {
       cache = require('../');
       cache.should.be.a.function;
     });
+
+    /** Calling that function should return a new instance of ExpressRedisCache */
 
     it ( 'should return a new ExpressRedisCache', function (done) {
       cache = cache({ prefix: prefix, host: host, port: port });
@@ -32,10 +38,16 @@
       });
     });
 
+    /** PROPERTIES */
+
+    /** {Object} cache.options */
+
     it ( 'should have a property options which is an object', function () {
       cache.should.have.property('options')
         .which.is.an.Object;
     });
+
+    /** {String} cache.prefix */
 
     it ( 'should have a property prefix which is a string and equals request prefix', function () {
       cache.should.have.property('prefix')
@@ -43,11 +55,15 @@
         .and.equal(prefix);
     });
 
+    /** {Object} cache.host */
+
     it ( 'should have a property host which is a string and equals request host', function () {
       cache.should.have.property('host')
         .which.is.a.String
         .and.equal(host);
     });
+
+    /** {Number} cache.port */
 
     it ( 'should have a property port which is a number and equals request port', function () {
       cache.should.have.property('port')
@@ -55,11 +71,15 @@
         .and.equal(port);
     });
 
+    /** {Number} cache.FOREVER */
+
     it ( 'should have a property FOREVER which is a number and equals -1', function () {
       cache.should.have.property('FOREVER')
         .which.is.a.Number
         .and.equal(-1);
     });
+
+    /** {Boolean} cache.connected */
 
     it ( 'should have a property connected which is a boolean and is true', function () {
       cache.should.have.property('connected')
@@ -67,11 +87,35 @@
         .and.is.true;
     });
 
+    /** {RedisClient} cache.client */
+
     it ( 'should have a property client which is a RedisClient', function () {
       cache.should.have.property('client')
         .which.is.an.Object;
 
       cache.client.constructor.name.should.equal('RedisClient');
+    });
+
+  });
+
+  /** Specify host and port */
+
+  describe ( 'Specifying host and port', function () {
+
+    before ( function (done) {
+      var cache = require('../');
+
+      var client1 = cache({ host: 'app.com', port: 1927 });
+
+      client1.on('error', done);
+
+      client1.on('connected', function () {
+        done();
+      });
+    });
+
+    it ( 'should be cool', function () {
+
     });
 
   });
