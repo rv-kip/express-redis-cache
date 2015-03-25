546070d99b916df1e45c165d788043c21ee8ec92
Patrick Williams <pwmckenna@gmail.com>
2015-03-25 11:45:14 -0700

    improving expire test coverage

--- test/ExpressRedisCache/add.js
+++ test/ExpressRedisCache/add.js
@@ -80,6 +80,13 @@
       setTimeout(function(){
         cache.get(_name, function (err, res) {
           should(err).not.be.ok;
+          res.should.be.an.Array.and.have.a.lengthOf(1);
+          done();
+        });
+      }, (cache.expire - 1) * 1000);
+      setTimeout(function(){
+        cache.get(_name, function (err, res) {
+          should(err).not.be.ok;
           res.should.be.an.Array.and.have.a.lengthOf(0);
           done();
         });
